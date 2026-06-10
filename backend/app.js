cf2ed1569792dd79965429040cfdfbdccf34fd78
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-06-10 21:17:45 +0530

    Server setup

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api", (req, res) => {
+    const data = {
+        message: "Hello from the server!"
+    };
+    res.send(data);
 });
 
 mongoose.connect(process.env.DB_PATH).then(() => {
   app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
-}).catch((err) => {
-  console.error("Failed to connect to the database", err);
 });
