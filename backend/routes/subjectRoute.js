2427455290d2667a054b4e64dfb95cb79f2ba124
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-06-10 22:32:18 +0530

    Routes

---
+++ backend/routes/subjectRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const subjectController = require("../controllers/subjectController");
+
+router.get("/", subjectController.getAllSubjects);
+router.post("/", subjectController.createSubject);
+router.put("/:id", subjectController.updateSubject);
+router.delete("/:id", subjectController.deleteSubject);
+
+module.exports = router;
