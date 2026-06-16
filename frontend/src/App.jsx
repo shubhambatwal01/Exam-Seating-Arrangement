cee131a8161198cb1624a74fdbcfec3b8512bdd8
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-06-16 12:38:15 +0530

    dashboard added in pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import Dashboard from "./pages/dashboard";
 
 function App() {
-  const [data, setData] = useState(0);
-
-  useEffect(() => {
-    axios
-      .get("/api")
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((err) => {
-        console.error("Error fetching data from the server", err);
-      });
-  }, []);
-
   return (
     <>
-    {data.message && data.message.map((message, index) => (
-      <h1 key={index}>{message}</h1>
-    ))}
+      <Dashboard />
     </>
   );
 }
