0c09b80c1196f5dd6244a758df87e92ac89a21c2
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-06-17 18:59:49 +0530

    facultyController added

---
+++ backend/controllers/facultyController.js
@@ -0,0 +1,30 @@
+const Faculty = require("../models/faculty");
+
+export const getFaculties = async (req, res) => {
+  try {
+    const faculties = await Faculty.find();
+    res.status(200).json(faculties);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createFaculty = async (req, res) => {
+  try {
+    const faculty = await Faculty.create(req.body);
+    res.status(201).json(faculty);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteFaculty = async (req, res) => {
+  try {
+    await Faculty.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Faculty deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
