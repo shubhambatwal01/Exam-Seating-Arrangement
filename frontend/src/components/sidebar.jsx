de9e23e8d9b1166605bacda41f4183a9a7660e73
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-06-17 13:29:06 +0530

    sidebar added

---
+++ frontend/src/components/sidebar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div>
+      <h2>Timetable Generator</h2>
+
+      <ul>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/subjects">Subjects</Link>
+        </li>
+        <li>
+          <Link to="/students">Students</Link>
+        </li>
+        <li>
+          <Link to="/faculties">Faculties</Link>
+        </li>
+        <li>
+          <Link to="/exam-settings">Exam Settings</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,7 +1,21 @@
+import Sidebar from "../components/Sidebar";
+
 function Dashboard() {
   return (
     <div>
-      <h1>Timetable Generator</h1>
+      <Sidebar />
+
+      <h1>Admin Dashboard</h1>
+
+      <div>
+        <h3>Total Students</h3>
+        <p>0</p>
+      </div>
+
+      <div>
+        <h3>Total Subjects</h3>
+        <p>0</p>
+      </div>
     </div>
   );
 }
