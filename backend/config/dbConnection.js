cb0e6629b13c1db95cef51acd3049aa70d0c7482
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-07-18 23:03:48 +0530

    Updated database config file

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+mongoose.set("strictQuery", false);
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_PATH);
-        console.log("Connected to MongoDB");
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await mongoose.connect(process.env.DB_PATH, {
+      connectTimeoutMS: 10000,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
