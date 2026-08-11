31b757818f9bc1ec85ff96eda70823cb66d6d02a
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-08-11 17:47:25 +0530

    student model modified

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-    prn: String,
-    name: String,
-    email: String,
-    courseId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course',
-        required: true,
+  prn: Number,
+  name: String,
+  email: String,
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  semesterId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Semester",
+    required: true,
+  },
+  subjects: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subject",
     },
-    semesterId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Semester',
-        required: true,
-    },
-    subjects: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Subject',
-    }],
+  ],
 });
 
 // Add an index on prn for faster lookups
 studentSchema.index({ prn: 1 }, { unique: true });
 
-
-module.exports = mongoose.model('Student', studentSchema);
+module.exports = mongoose.model("Student", studentSchema);
