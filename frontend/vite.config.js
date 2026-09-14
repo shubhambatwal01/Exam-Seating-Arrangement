54ab0ef59032347f7090a3af8ae85dcd2cab3ce0
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-09-14 19:53:29 +0530

    add tailwindcss, postcss & modify vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:1101",
-    },
-  },
-});
+export default defineConfig({ plugins: [react()], server: { port: 5173 } });
