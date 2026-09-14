b367d87fa143e631465acb84aa7289bce830b158
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-09-14 19:47:09 +0530

    add header to website

---
+++ frontend/src/components/CollegeHeader.jsx
@@ -0,0 +1,11 @@
+export default function CollegeHeader() {
+  return (
+    <div className="w-full bg-[#034568]">
+      <img
+        src="Public/ModernCollegeLogo.png"
+        alt="Modern College of Arts, Science & Commerce"
+        className="block h-auto max-w-full"
+      />
+    </div>
+  );
+}
