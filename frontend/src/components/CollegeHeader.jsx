a093698217d5e22eb09ee79fb2447602964b178f
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-09-17 13:33:47 +0530

    add link of icons

--- frontend/src/components/CollegeHeader.jsx
+++ frontend/src/components/CollegeHeader.jsx
@@ -2,7 +2,7 @@ export default function CollegeHeader() {
   return (
     <div className="w-full bg-[#034568]">
       <img
-        src="Public/ModernCollegeLogo.png"
+        src="https://i.8upload.com/image/def6683b13ad2724/moderncollegelogo.png"
         alt="Modern College of Arts, Science & Commerce"
         className="block h-auto max-w-full"
       />
