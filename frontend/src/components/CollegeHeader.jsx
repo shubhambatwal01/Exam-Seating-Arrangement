a7043eb3ba28a2a0490f765d14083584d7b455cf
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-09-25 16:00:00 +0530

    modify some style in header

--- frontend/src/components/CollegeHeader.jsx
+++ frontend/src/components/CollegeHeader.jsx
@@ -1,8 +1,8 @@
 export default function CollegeHeader() {
   return (
-    <div className="w-full bg-[#034568]">
+    <div className="w-full border-b border-cyan-900/20 bg-[#034568]">
       <img
-        src="https://i.8upload.com/image/def6683b13ad2724/moderncollegelogo.png"
+        src="/ModernCollegeLogo.png"
         alt="Modern College of Arts, Science & Commerce"
         className="block h-auto max-w-full"
       />
